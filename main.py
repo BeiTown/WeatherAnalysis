6d7c79c5d99b0e4879ddc15bb43d799587b18765
BeiTown <beitown@qq.com>
2019-02-19 18:24:20 +0800

    0219

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(1)
+import re
+
+stationID = 'CHM00057679'
+
+# 导入数据
+with open('data/'+ stationID +'.dly', 'r') as f1:
+    dataList = f1.readlines()
+
+print(dataList[2])
+
+
+
+# re.split() 对字符串进行分割操作
+# res=re.split("\n|s|-| +",dataList[3])
+res=re.split("\s+|s|-|s\s+",dataList[2])
+
+print(res)
