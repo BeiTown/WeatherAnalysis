6862d791102a5872a5e00af386e396cb41593ec6
BeiTown <beitown@qq.com>
2019-02-21 18:10:55 +0800

    Create

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 from matplotlib.ticker import MultipleLocator,FuncFormatter
 import math
 
+ISTEST = True
 
 def hsv2rgb(h, s, v):
     h = float(h)
@@ -47,7 +48,7 @@ def month_formatter(x,pos):
         return v
 
 stationID = 'CHM00057679'
-df_PRCP = DataFrame(columns=['datetime', 'year', 'month', 'day', 'dayIndex', 'value'])
+df_PRCP = DataFrame(columns=['datetime', 'year', 'month', 'day', 'dayIndex', 'value', 'color'])
 df_TAVG = DataFrame(columns=['datetime', 'year', 'month', 'day', 'dayIndex', 'value', 'color'])
 df_TMAX = DataFrame(columns=['datetime', 'year', 'month', 'day', 'dayIndex', 'value'])
 df_TMIN = DataFrame(columns=['datetime', 'year', 'month', 'day', 'dayIndex', 'value'])
@@ -57,6 +58,8 @@ tMaxTime = ''
 tMin = 0
 tMinTime = ''
 
+pMax = 0
+pMaxTime = ''
 
 # 导入数据
 with open('data/'+ stationID +'.dly', 'r') as f1:
@@ -77,8 +80,7 @@ for _d in dataList:
     for i in range(1, len(_date_res)):
         _datetime = str(_year)+'-' + str(_month) + '-'+str(i)
 
-
-
+        # 降雨记录
         if _type == 'PRCP':
 
             if _month == '01' and i == 1:
@@ -86,11 +88,20 @@ for _d in dataList:
             else:
                 pdi += 1
 
-            _value = str(_date_res[i]).find('T') == True and -9999 or _date_res[i]
-            _df = DataFrame({'datetime':[_datetime], 'year':[_year],'month':[_month],'day':[i], 'dayIndex':[pdi],'value':[_value]})
+            _value = float(_date_res[i])  / 10
+
+            if _value > pMax:
+                pMax = _value
+                pMaxTime = _year + '/' + _month + '/' + str(i)
+
+            _color = '#FFFFFF'
+            if _value != -999.9:
+                _color = color(tuple(hsv2rgb(int(_value * 0.75) + 180, 1, 1)))
+
+            _df = DataFrame({'datetime':[_datetime], 'year':[_year],'month':[_month],'day':[i], 'dayIndex':[pdi],'value':[_value],'color':[_color]})
             df_PRCP = df_PRCP.append(_df, ignore_index=True)
 
-
+        # 平均气温记录
         elif _type == 'TAVG':
 
             if _month == '01' and i == 1:
@@ -102,14 +113,11 @@ for _d in dataList:
 
             _color = '#FFFFFF'
             if _value != -999.9:
-
                 _color = color(tuple(hsv2rgb(int(-7 * _value) + 240, 1, 1)))
-
-
-
             _df = DataFrame({'datetime':[_datetime], 'year':[_year],'month':[_month],'day':[i], 'dayIndex':[tdi],'value':[_value],'color':[_color]})
             df_TAVG = df_TAVG.append(_df, ignore_index=True)
 
+        # 最高气温记录
         elif _type == 'TMAX':
 
             _value = str(_date_res[i]).find('T') == True and 0 or float(_date_res[i]) / 10
@@ -117,6 +125,7 @@ for _d in dataList:
                 tMax = _value
                 tMaxTime = _year + '/' + _month + '/' + str(i)
 
+        # 最低气温记录
         elif _type == 'TMIN':
 
             _value = str(_date_res[i]).find('T') == True and 0 or float(_date_res[i]) / 10
@@ -124,25 +133,50 @@ for _d in dataList:
                 tMin = _value
                 tMinTime = _year + '/' + _month + '/' + str(i)
 
-
     # 调试使用，避免数据加载时间过长
-    if len(df_PRCP) and len(df_TAVG) > 100000:
+    if len(df_PRCP) and len(df_TAVG) > 1000:
         break
+
+# 柱状图
+
+# 年连续干旱最长时间段
+
+# 年连续降雨最长时间段
+
+# 年连续高温最长时间段
+
+# 年连续低温最长时间段
+
+
+# 年干旱天数统计
+#t = df_TAVG[df_TAVG['value'] > 0].groupby('year')
+#print(t)
+# 年降雨天数统计
+
+
+# 年高温天数统计
+# 年低温天数统计
 
 
 #print(df_PRCP)
-df_TAVG.reset_index(drop=True)
-print(df_TAVG)
-
+#df_TAVG.reset_index(drop=True)
+#print(df_TAVG)
+print(df_PRCP)
+t = df_PRCP.set_index('datetime')
+print(t)
 # 365天的散点图
 
-#plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))  #設置x軸主刻度顯示格式（日期）
-#plt.gca().xaxis.set_major_locator(mdates.MonthLocator(interval=2))  #設置x軸主刻度間距
-#plt.gca().xaxis.set_minor_locator(mdates.DayLocator())
 
-# T = np.arctan2(df_TAVG['dayIndex'], df_TAVG['value']) # for color value
+# 画布基础设置
+if ISTEST == False:
+    plt.rcParams['figure.figsize'] = (12.0, 8.0) # 设置figure_size尺寸
+    plt.rcParams['image.interpolation'] = 'nearest' # 设置 interpolation style
+    plt.rcParams['image.cmap'] = 'gray' # 设置 颜色 style
 
+    plt.rcParams['savefig.dpi'] = 300 #图片像素
+    plt.rcParams['figure.dpi'] = 300 #分辨率
 
+# TAVG
 plt.gca().xaxis.set_major_locator(MultipleLocator(31))
 plt.gca().xaxis.set_major_formatter(FuncFormatter(month_formatter))
 
@@ -150,15 +184,67 @@ plt.gca().grid()# 网格
 # plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
 plt.scatter(df_TAVG['dayIndex'], df_TAVG['value'], s=80, c=df_TAVG['color'], marker='.', alpha=0.05)
 
-plt.axis()
+
 plt.ylim(-30, 50)
 
-plt.title('TAVG')
+plt.title('TAVG : 1951-2019')
 plt.xlabel("Month")
 plt.ylabel("Temperature C°")
-plt.show()
 
+
+if ISTEST == False:
+    plt.savefig("./data/TAVG.png",dpi=300)
+
+plt.show()
 print('TMAX = '+ str(tMax) + 'C :' + tMaxTime)
 print('TMIN = '+ str(tMin) + 'C :' + tMinTime)
+
+
+
+# PRCP
+plt.gca().xaxis.set_major_locator(MultipleLocator(31))
+plt.gca().xaxis.set_major_formatter(FuncFormatter(month_formatter))
+plt.gca().yaxis.set_major_locator(plt.MultipleLocator(10))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, y: y * 10))
+
+plt.gca().grid()# 网格
+#plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.scatter(df_PRCP['dayIndex'], df_PRCP['value'], s=40, c=df_PRCP['color'], marker='.', alpha=0.5)
+
+plt.ylim(1, 250)
+
+plt.title('PRCP : 1951-2019')
+plt.xlabel("Month")
+plt.ylabel("Precipitation mm")
+
+if ISTEST == False:
+    plt.savefig("./data/PRCP.png",dpi=300)
+
+plt.show()
+
+print('PMAX = '+ str(pMax) + 'mm :' + pMaxTime)
+
+# PCRP补充雨季显示
+
+plt.gca().xaxis.set_major_locator(MultipleLocator(31))
+plt.gca().xaxis.set_major_formatter(FuncFormatter(month_formatter))
+plt.gca().yaxis.set_major_locator(plt.MultipleLocator(5))
+plt.gca().yaxis.set_major_formatter(FuncFormatter(lambda x, y: y * 5))
+
+plt.gca().grid()# 网格
+#plt.setp(plt.gca().get_xticklabels(), rotation=45, horizontalalignment='right')
+plt.scatter(df_PRCP['dayIndex'], df_PRCP['value'], s=40, c='#00CCFF', marker='.', alpha=0.1)
+
+plt.ylim(1, 50)
+
+plt.title('Rainy Season Show : 1951-2019')
+plt.xlabel("Month")
+plt.ylabel("Precipitation mm")
+
+if ISTEST == False:
+    plt.savefig("./data/PRCP_RAINDAY.png",dpi=300)
+
+plt.show()
+
 
 # 12 x 31 的3D散点图
